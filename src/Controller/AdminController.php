abf7ffb83fc3528c954dad41b016de9e5a9f801c
Elsa Rhudye Mangoye <diamant.rhudye@gmail.com>
2021-06-22 10:45:17 +0200

    installation et cobfig cheditor, modification admin post, mise en forme clean

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Post;
 use App\Form\CategoryType;
+use App\Form\PostType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -34,6 +36,26 @@ class AdminController extends AbstractController
         }
         return $this->render('admin/category/add.html.twig', [
             'form' => $form->createView(),
+            'bg_image' => 'clean/assets/img/about-bg.jpg'
+        ]);
+    }
+
+    #[Route('/post/add', name: 'post_add')]
+    public function addPost(Request $request): Response
+    {
+        $category = new Post();
+        $form = $this->createForm(PostType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute('admin_home');
+        }
+        return $this->render('admin/post/add.html.twig', [
+            'form' => $form->createView(),
+            'bg_image' => 'clean/assets/img/post-bg.jpg'
         ]);
     }
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,12 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Post;
-use Doctrine\DBAL\Types\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostType extends AbstractType
 {
@@ -17,14 +21,15 @@ class PostType extends AbstractType
         $builder
             ->add('title', TextType::class)
             //->add('slug')
-            ->add('content', TextareaType::class)
+            ->add('content', CKEditorType::class)
             ->add('image', TextType::class)
             //->add('createdAt')
             //->add('active')
             //->add('user')
             ->add('category', EntityType::class, [
                 'class' => Category::class
-            ]);
+            ])
+            ->add('Valider', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
